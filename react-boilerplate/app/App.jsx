59a7a67c631c19c803d08505e8f21958949d4e94
miguelMirai <miguel.arroyo@miraiespana.com>
2017-06-22 18:06:06 +0200

    Add local scope to bootstrap stylesheet

--- react-boilerplate/app/App.jsx
+++ react-boilerplate/app/App.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import {render} from 'react-dom';
-import './css/style.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import MainComponent from './components/MainComponent.jsx';
 
 render(
     <MainComponent/>, 
     document.getElementById('app')
 );
-
